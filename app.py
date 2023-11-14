3bf578a2fe338d8321d77e98613811639c04b7b4
Vito <leonev02@moravian.edu>
2023-11-14 09:55:14 -0500

    Gets top 10 gainers with ticker and % change

--- app.py
+++ app.py
@@ -4,7 +4,13 @@ import requests
 response = requests.get('https://www.google.com/finance/markets/gainers')
 soup = BeautifulSoup(response.text, 'html.parser')
 
-stocks = soup.find('div', class_="Sy70mc").find_all('div', class_='JwB6zf')
+stocks = soup.find('div', class_="Sy70mc").find_all('li')
 
+counter = 0
 for stock in stocks:
-    print(stock.text)
+    name = stock.find('div', class_='COaKTb').text
+    change = stock.find('div', class_='JwB6zf').text
+    print(name, change)
+    counter += 1
+    if counter == 10:
+        break
